a272abab09b4cd887cc4ea3dd10c8dfcccd0901e
willcooper865 <willcooper865@yahoo.com>
2021-07-29 11:39:10 -0400

    multiple set 0 settimeouts

--- code.js
+++ code.js
@@ -1,18 +1,17 @@
-
 // Synchronous code
-
+"synchronous code means code which executes in sequence"
 // "sync": together, aligned, sequential, matched up, step-by-step, harmony, accord
 // "chrono": time
 
-"synchronous code means code which executes in sequence"
-
-console.log("1. first")
-setTimeout(() => console.log("5. fifth"), 5000)
-setTimeout(() => console.log("3. third"), 0)
-setTimeout(() => console.log("4. fourth"), 1000)
-console.log("2. second")
-
 // Asynchronous code
 // "A-" means "not"
-// setInterval() 
 "asynchronous code means code which does not execute in sequence"
+
+
+console.log("1. first")
+setTimeout(() => console.log("7. seventh"), 5000)
+setTimeout(() => console.log("3. third"), 0)
+setTimeout(() => console.log("4. fourth"), 0)
+setTimeout(() => console.log("5. fifth"), 0)
+setTimeout(() => console.log("6. sixth"), 1000)
+console.log("2. second")
